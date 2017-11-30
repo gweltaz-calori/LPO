3c7648a150a2b06ac4d75d50af494a3ec5e06d9e
gwel.74@formation-cci.fr <gwel.74@formation-cci.fr>
2017-11-30 11:13:27 +0100

    client route

--- client/index.php
+++ client/index.php
@@ -1,15 +1,37 @@
 <?php
 require_once( './autoload.php');
+require_once __DIR__ . '/vendor/autoload.php';
 
-$api_instance = new Swagger\Client\Api\AdherentsApi();
+/*$api_instance = new Swagger\Client\Api\AdherentsApi();
 $id = 56; // int |
 
 try {
     $result = $api_instance->getAdherent($id);
 
-    print_r($result->getNom());
+    print_r($result);
 } catch (Exception $e) {
     echo 'Exception when calling AdherentsApi->getAdherent: ', $e->getMessage(), PHP_EOL;
-}
+}*/
+/*echo "ag";*/
 
-?>
+
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
+
+$app->GET('/nichoirs', function ($request, $response, $args) {
+
+    return $response->write("ok");
+
+});
+
+$app->GET('/ah', function ($request, $response, $args) {
+
+    return $response->write("test");
+});
+$app->run();
