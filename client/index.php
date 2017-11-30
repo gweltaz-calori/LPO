6eee516a28c5a6522b915269291ffdf5acd04b44
shotPinguin <chanet.thomas74@gmail.com>
2017-11-30 11:13:04 +0100

    vue nichoirs

--- client/index.php
+++ client/index.php
@@ -1,15 +1,25 @@
 <?php
+
+require_once( './autoload.php');
+require_once "./vendor/autoload.php";
+
+$thecontroler = new \Swagger\Client\controler\controleurNichoir();
+$thecontroler->indexAction();
+
+/*
 require_once( './autoload.php');
 
 $api_instance = new Swagger\Client\Api\AdherentsApi();
-$id = 56; // int |
+$id = 1; // int |
 
 try {
+    $test = new Swagger\Client\Api\NichoirsApi();
+    $test->getAllNichoir();
     $result = $api_instance->getAdherent($id);
 
-    print_r($result->getNom());
+    var_dump($result);
 } catch (Exception $e) {
     echo 'Exception when calling AdherentsApi->getAdherent: ', $e->getMessage(), PHP_EOL;
 }
-
+*/
 ?>
