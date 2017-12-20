4e8716df451c4037eca9c52ac550a6c2a0a946a1
gweltaz-calori <andoracontact@gmail.com>
2017-12-20 21:14:01 +0100

    fix fixtures

--- server/lib/DataFixtures/LoadAdherentData.php
+++ server/lib/DataFixtures/LoadAdherentData.php
@@ -17,6 +17,7 @@ class LoadAdherentData implements FixtureInterface
 
         $userRepo = $manager->getRepository('Models\Adherent');
         $userExist = $userRepo->findOneBy(['login' => "polo"]);
+        $userExist2 = $userRepo->findOneBy(['login' => "cena"]);
 
         if(!$userExist) {
             $adherent = new Adherent();
@@ -34,11 +35,32 @@ class LoadAdherentData implements FixtureInterface
             $adherent->setAdresse($adresse);
             $adherent->setPassword('polo');
             $adherent->setPhoto('http://static4.businessinsider.com/image/57ae527ddb5ce9b2008b6935/paul-szoldra.jpg');
-
             $manager->persist($adherent);
-            $manager->flush();
         }
 
+        if(!$userExist2) {
+
+            $adherent2 = new Adherent();
+            $adherent2->setNom('Cena');
+            $adherent2->setPrenom('John');
+            $adherent2->setLogin('cena');
+            $adherent2->setTelephone('555556');
+            $adherent2->setMail('johncena@gmail.com');
+
+            $adresse2 = new Adresse();
+            $adresse2->setCp('74960');
+            $adresse2->setRue('2 rue de cena');
+            $adresse2->setVille('New York');
+
+            $adherent2->setAdresse($adresse2);
+            $adherent2->setPassword('cena');
+            $adherent2->setPhoto('https://upload.wikimedia.org/wikipedia/commons/7/77/John_Cena_May_2016.jpg');
+
+            $manager->persist($adherent2);
+        }
+
+
+        $manager->flush();
 
 
 
